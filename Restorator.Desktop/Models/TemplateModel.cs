89c82e0aa2afccff69514e3990bbb81eb3553568
a.panfilov <a.panfilov@uac-icc>
2025-01-11 14:17:53 +0400

    added template maker

---
+++ Restorator.Desktop/Models/TemplateModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Restorator.Desktop.Models
+{
+    public class TemplateModel
+    {
+        public string Filename { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
--- Restorator.Desktop/ViewModels/RestaurantTemplateGeneratorViewModel.cs
+++ Restorator.Desktop/ViewModels/RestaurantTemplateGeneratorViewModel.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.ObjectModel;
 using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Restorator.Desktop.Models;
@@ -10,15 +13,22 @@ namespace Restorator.Desktop.ViewModels
     public partial class RestaurantTemplateGeneratorViewModel : ViewModelBase
     {
         private const string SchemeLocation = "C:\\Users\\user\\source\\repos\\XaKer547\\Restorator\\Restorator.Seeder\\Resources\\RestaurantsPlan";
+        //тут свой путь к сидеру пиши
 
         [ObservableProperty]
         private ObservableCollection<TableModel> tables = [];
 
         [ObservableProperty]
-        private ObservableCollection<byte[]> templates = [];
+        private ObservableCollection<TemplateModel> templates = [];
 
         [ObservableProperty]
         private TableModel? selectedTable;
+
+        [ObservableProperty]
+        private TemplateModel selectedTemplate;
+
+        [ObservableProperty]
+        private string seederScript;
 
         [RelayCommand]
         public void Initialize()
@@ -33,7 +43,7 @@ namespace Restorator.Desktop.ViewModels
 
             foreach (var image in images)
             {
-                Templates.Add(GetByteArrayFromImage(image));
+                Templates.Add(GetTemplateFromPath(image));
             }
         }
 
@@ -48,8 +58,8 @@ namespace Restorator.Desktop.ViewModels
         {
             var table = new TableModel()
             {
-                X = 366.09,
-                Y = 592.63,
+                X = 0,
+                Y = 0,
                 State = Domain.Models.Enums.TableStates.OccupiedByUser,
                 Rotation = 0,
                 Height = 183,
@@ -69,15 +79,45 @@ namespace Restorator.Desktop.ViewModels
             SelectedTable = null;
         }
 
-        private byte[] GetByteArrayFromImage(string imagePath)
+        private TemplateModel GetTemplateFromPath(string imagePath)
         {
-            var stream = File.OpenRead(imagePath);
+            return new TemplateModel()
+            {
+                Filename = Path.GetFileName(imagePath),
+                Content = File.ReadAllBytes(imagePath),
+            };
+        }
 
-            using var memoryStream = new MemoryStream();
+        [RelayCommand]
+        public void CopyScriptToBuffer() => Clipboard.SetText(SeederScript);
 
-            stream.CopyTo(memoryStream);
+        [RelayCommand]
+        public void GenerateSeederScript()
+        {
+            var script = new StringBuilder("new RestaurantTemplate\n{\n");
 
-            return memoryStream.ToArray();
+            var fileNameWithoutExtension = SelectedTemplate.Filename.Split('.')[0];
+
+            script.AppendLine($"\tImage = EmbeddedResourceHelper.GetRestaurantPlan(\"{fileNameWithoutExtension}\"),");
+
+            script.AppendLine("\tTables = new List<Table>()\n\t\t{");
+
+            foreach (var table in Tables)
+            {
+                script.AppendLine("\t\t\tnew Table()\n\t\t\t{");
+
+                var templateId = table.Width == 183 && table.Height == 183 ? 1 : 2;
+
+                script.AppendLine($"\t\t\t\tTableTemplateId ={templateId}");
+                script.AppendLine($"\t\t\t\tX = {table.X + 10},"); //у меня margin стоит для того чтобы FlipView отработал
+                script.AppendLine($"\t\t\t\tY = {table.Y}");
+                script.AppendLine("\t\t\t},");
+            }
+
+            script.AppendLine("\t\t}");
+            script.AppendLine("}");
+
+            SeederScript = script.ToString();
         }
     }
 }
