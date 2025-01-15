dbfea1d0a72ce9128d966ff1dc2df187ac2f8085
7Kaika7 <146513055+7Kaika7@users.noreply.github.com>
2025-01-15 19:49:29 +0400

    teg and rest

--- Restorator.Seeder/Data/DbSeeder/RestaurantSeeder.cs
+++ Restorator.Seeder/Data/DbSeeder/RestaurantSeeder.cs
@@ -131,7 +131,7 @@ namespace Restorator.Seeder.Data.DbSeeder
                         "Ресторан «Kinza» предлагает своим гостям изысканные блюда, такие как хачапури, хинкали, салаты, паста и пицца, а также фирменные напитки, такие как чай с брусникой и специями.",
                     TemplateId = 5,
                     Image = EmbeddedResourceHelper.GetRestaurantImage("Kinza"), 
-                    //MenuImage = EmbeddedResourceHelper.GetRestaurantMenu("Kinza"), // я не вижу меню
+                    MenuImage = EmbeddedResourceHelper.GetRestaurantMenu("Kinza"), 
                     BeginWorkTime = new TimeOnly(12, 0),
                     EndWorkTime = new TimeOnly(0, 0),
                     Approved = true,
