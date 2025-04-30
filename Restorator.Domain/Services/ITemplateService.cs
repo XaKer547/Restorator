37c59ceabb294e157724358eaf628267dd98721c
XaKer_547 <a.panfilov@225-IT-PC>
2025-04-30 09:03:02 +0400

    a

---
+++ Restorator.Domain/Services/ITemplateService.cs
@@ -0,0 +1,16 @@
+﻿using FluentResults;
+using Restorator.Domain.Models.Restaurant;
+
+namespace Restorator.Domain.Services
+{
+    public interface ITemplateService
+    {
+        Task<IReadOnlyCollection<RestaurantTemplateDTO>> GetRestaurantTemplates();
+        //Task<IReadOnlyCollection<Table> GetTableTemplates(); TableTemplateDTO
+        //id
+        //type
+        //size
+        //no rotate
+        Task<Result> CreateRestaurantTemplate();
+    }
+}
